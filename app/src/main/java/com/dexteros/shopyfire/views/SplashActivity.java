27e01f6ff39042f673e93e0f40ae5e0d69bfc61b
Juan Manuel Lora <juanm.lora@premize.com>
2016-06-08 22:47:33 -0500

    Implementacion FAB Button (test)

---
+++ app/src/main/java/com/dexteros/shopyfire/views/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.dexteros.shopyfire.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dexteros.shopyfire.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
